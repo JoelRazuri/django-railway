21d23272837586893662fcbb38c74285e6aec784
Joel Razuri <jrazuri98@hotmail.com>
2024-03-21 20:34:46 -0300

    postgresql

--- class_views/settings.py
+++ class_views/settings.py
@@ -68,11 +68,11 @@ WSGI_APPLICATION = 'class_views.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mydatabase",
-        "USER": "mydatabaseuser",
-        "PASSWORD": "mypassword",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "NVpObwURLJmjfPkyGzfajodYlgZBuyyU",
+        "HOST": "roundhouse.proxy.rlwy.net",
+        "PORT": "33057",
     }
 }
 
